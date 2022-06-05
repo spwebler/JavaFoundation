78cd7191a61e7201cc6363e6ac64676b6ab99883
Shraddha Prabhu <66193376+spwebler@users.noreply.github.com>
2022-06-05 23:21:16 +0530

    Add files via upload

---
+++ Patterns/pattern2.java
@@ -0,0 +1,24 @@
+package pattern1.java;
+
+public class pattern2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for(int i=1;i<=4;i++)
+{
+	for(int j=1;j<=5;j++)
+	{
+		if(i==1 || i==4 || j==1||j==5)
+		{
+			System.out.print("*");
+		}
+		else
+		{
+			System.out.print(" ");
+		}
+	}
+	System.out.println(" ");
+	}
+
+}
+}
